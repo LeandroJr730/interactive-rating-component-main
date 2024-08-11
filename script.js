8d1092c7fbe9496605ba102b17c69b5717cb3ef8
Leandro Junior <juninhoromagnoli11@gmail.com>
2024-08-10 21:58:35 -0500

    projected finished
    still need to adjust the desktop and mobile versions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const rateContainer = document.querySelector(".rating-container");
+const thankYouContainer = document.querySelector(".thank-you-container");
+const submitBtn = document.querySelector(".submit-button");
+const buttons = document.querySelectorAll(".btn");
 const rating = document.getElementById("rating");
+
+submitBtn.addEventListener("click", () => {
+  if (rating.textContent === "") {
+    window.alert("Please select a rating");
+  } else {
+    thankYouContainer.classList.remove("hidden");
+    rateContainer.style.display = "none";
+  }
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    rating.textContent = button.value;
+  });
+});
